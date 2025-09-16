bdbee23b89ef9fbda47557b05073dfc40fd57fba
simon <s.tretter@gmail.com>
2025-09-16 22:51:43 +0200

    promise never resolves

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,27 @@
+import { QueryClient } from '@tanstack/react-query'
+import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query'
 import { createRouter as createReactRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
 export function createRouter() {
-  return createReactRouter({
+    const queryClient = new QueryClient()
+
+    const router =createReactRouter({
     routeTree,
     context: {
       head: '',
+       // queryClient
     },
     defaultPreload: 'intent',
     scrollRestoration: true,
   })
+
+    setupRouterSsrQueryIntegration({
+        router,
+        queryClient,
+    })
+
+    return router
 }
 
 declare module '@tanstack/react-router' {
